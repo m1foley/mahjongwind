ae7da96e86badfb7e2173de68a67f3fc9482560b
Michael Foley <foley3@gmail.com>
2021-01-31 21:42:46 -0800

    Persist wind tiles for first deal roll

--- lib/mjw_web/live/game_live/dice_component.ex
+++ lib/mjw_web/live/game_live/dice_component.ex
@@ -11,6 +11,21 @@ defmodule MjwWeb.GameLive.DiceComponent do
     {:ok, socket}
   end
 
+  @doc """
+  Special handler for the very first deal roll, so the wind tiles don't
+  disappear too quickly
+  """
+  @impl true
+  def handle_event("roll-first-deal", _params, socket) do
+    game_state = socket.assigns.game_state
+
+    socket =
+      socket
+      |> persist_roll(game_state, :rolled_for_first_deal)
+
+    {:noreply, socket}
+  end
+
   @impl true
   def handle_event("roll", _params, socket) do
     game_state = socket.assigns.game_state
@@ -31,11 +46,11 @@ defmodule MjwWeb.GameLive.DiceComponent do
     socket
   end
 
-  defp persist_roll(socket, :rolling_for_deal) do
+  defp persist_roll(socket, :rolling_for_deal, event) do
     socket.assigns.game
     |> Mjw.Game.roll_dice()
     |> Mjw.Game.deal()
-    |> MjwWeb.GameStore.update()
+    |> MjwWeb.GameStore.update(event)
 
     socket
   end
