9f35c11f91a1163960507bd315b62a3e45ca4956
Michael Foley <foley3@gmail.com>
2021-02-13 00:21:26 -0800

    Extract game menu into component
    Only reset and quit can be handled there so far, but this small
    extraction is better than nothing.

--- lib/mjw_web/live/game_live/show.ex
+++ lib/mjw_web/live/game_live/show.ex
@@ -72,33 +72,6 @@ defmodule MjwWeb.GameLive.Show do
     {:noreply, socket}
   end
 
-  @impl true
-  def handle_event("quit", _params, socket) do
-    current_user_sitting_at = socket.assigns.current_user_sitting_at
-    seat = socket.assigns.relative_game_seats |> Enum.at(0)
-
-    socket.assigns.game
-    |> Mjw.Game.evacuate_seat(current_user_sitting_at)
-    |> MjwWeb.GameStore.update(:left_game, %{seat: seat})
-
-    socket =
-      socket
-      |> push_redirect(to: Routes.game_index_path(socket, :index))
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("reset", _params, socket) do
-    player_seat = socket.assigns.relative_game_seats |> Enum.at(0)
-
-    socket.assigns.game
-    |> Mjw.Game.reset()
-    |> MjwWeb.GameStore.update(:reset, %{seat: player_seat})
-
-    {:noreply, socket}
-  end
-
   # sorting one's own concealed tiles
   @impl true
   def handle_event(
