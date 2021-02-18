87de6ca32cd268c2799d92c75d0b18cb82d2fabd
Michael Foley <foley3@gmail.com>
2021-02-17 19:56:19 -0800

    Extract seed_test_game, hide behind param

--- lib/mjw_web/live/game_live/index.ex
+++ lib/mjw_web/live/game_live/index.ex
@@ -2,22 +2,8 @@ defmodule MjwWeb.GameLive.Index do
   use MjwWeb, :live_view
 
   @impl true
-  def mount(_params, session, socket) do
-    # seed with test game
-    test_game = Mjw.Game.new() |> Map.merge(%{id: "test1"})
-
-    if !MjwWeb.GameStore.get("test1") do
-      test_game
-      |> Map.merge(%{
-        seats: [
-          %Mjw.Seat{player_id: "id0", player_name: "Snoop Dogg", picked_wind: "ww"},
-          %Mjw.Seat{player_id: "id1", player_name: "Dr. Dre", picked_wind: "wn"},
-          %Mjw.Seat{},
-          %Mjw.Seat{}
-        ]
-      })
-      |> MjwWeb.GameStore.update(:test_init)
-    end
+  def mount(params, session, socket) do
+    if params["test"], do: seed_test_game()
 
     socket =
       socket
@@ -46,5 +32,22 @@ defmodule MjwWeb.GameLive.Index do
       |> Enum.reject(&Mjw.Game.empty?/1)
 
     assign(socket, :games, seated_games)
+  end
+
+  def seed_test_game() do
+    test_game = Mjw.Game.new() |> Map.merge(%{id: "test1"})
+
+    unless MjwWeb.GameStore.get("test1") do
+      test_game
+      |> Map.merge(%{
+        seats: [
+          %Mjw.Seat{player_id: "snoop", player_name: "Snoop Dogg", picked_wind: "ww"},
+          %Mjw.Seat{player_id: "dre", player_name: "Dr. Dre", picked_wind: "wn"},
+          %Mjw.Seat{},
+          %Mjw.Seat{}
+        ]
+      })
+      |> MjwWeb.GameStore.update(:test_init)
+    end
   end
 end
