1bfe2eed879a8a3c6bc1f3e2686c99b75c80fede
Michael Foley <foley3@gmail.com>
2021-07-08 15:40:42 -0700

    Remove test game seeding

--- lib/mjw_web/live/game_live/index.ex
+++ lib/mjw_web/live/game_live/index.ex
@@ -2,9 +2,7 @@ defmodule MjwWeb.GameLive.Index do
   use MjwWeb, :live_view
 
   @impl true
-  def mount(params, session, socket) do
-    if params["test"], do: seed_test_game()
-
+  def mount(_params, session, socket) do
     socket =
       socket
       |> assign_defaults(session)
@@ -27,27 +25,7 @@ defmodule MjwWeb.GameLive.Index do
   end
 
   defp fetch_games(socket) do
-    seated_games =
-      MjwWeb.GameStore.all()
-      |> Enum.reject(&Mjw.Game.empty?/1)
-
+    seated_games = Enum.reject(MjwWeb.GameStore.all(), &Mjw.Game.empty?/1)
     assign(socket, :games, seated_games)
-  end
-
-  def seed_test_game() do
-    test_game = Mjw.Game.new() |> Map.merge(%{id: "test1"})
-
-    unless MjwWeb.GameStore.get("test1") do
-      test_game
-      |> Map.merge(%{
-        seats: [
-          %Mjw.Seat{player_id: "snoop", player_name: "Snoop Dogg", picked_wind: "ww"},
-          %Mjw.Seat{player_id: "dre", player_name: "Dr. Dre", picked_wind: "wn"},
-          %Mjw.Seat{},
-          %Mjw.Seat{}
-        ]
-      })
-      |> MjwWeb.GameStore.update(:test_init)
-    end
   end
 end
