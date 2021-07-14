0de4add4756d9b78d0412f8279841a20054badca
Michael Foley <foley3@gmail.com>
2021-07-13 23:25:15 -0700

    Refactor authentication code

--- lib/mjw_web/plugs/authentication.ex
+++ lib/mjw_web/plugs/authentication.ex
@@ -5,26 +5,29 @@ defmodule MjwWeb.Plugs.Authentication do
   end
 
   def call(conn, _params) do
-    conn |> fetch_session |> ensure_user_id |> assign_curent_user_id
+    conn
+    |> fetch_session()
+    |> ensure_user_id()
+    |> assign_curent_user_id()
   end
 
   defp ensure_user_id(conn) do
-    if session_user_id(conn) do
+    if get_session_user_id(conn) do
       conn
     else
       create_session_user_id(conn)
     end
   end
 
-  defp session_user_id(conn) do
-    conn |> get_session(:user_id)
+  defp get_session_user_id(conn) do
+    get_session(conn, :user_id)
   end
 
   def create_session_user_id(conn) do
-    conn |> put_session(:user_id, UUID.uuid4())
+    put_session(conn, :user_id, UUID.uuid4())
   end
 
   def assign_curent_user_id(conn) do
-    conn |> assign(:current_user_id, session_user_id(conn))
+    assign(conn, :current_user_id, get_session_user_id(conn))
   end
 end
