5c32764b3a6ee510db06f3e6bd5fe9d2cbcefacc
Michael Foley <foley3@gmail.com>
2024-07-07 19:00:50 -0700

    Extract current_user_seat into component

--- lib/mjw_web/components/game_components.ex
+++ lib/mjw_web/components/game_components.ex
@@ -114,4 +114,66 @@ defmodule MjwWeb.GameComponents do
     </div>
     """
   end
+
+  # seat is a decorator with extra attributes around Mjw.Seat
+  attr(:seat, :map, required: true)
+  attr(:current_user_drawing, :boolean, required: true)
+  attr(:win_declared_seatno, :integer, required: true)
+  attr(:current_user_seatno, :integer, required: true)
+  attr(:current_user_discarding, :boolean, required: true)
+  attr(:available_discard_tile, :boolean, required: true)
+
+  def current_user_seat(assigns) do
+    ~H"""
+    <div id="seat-0">
+      <div class={"tiles flex-wrap turn-glow-0-#{if @current_user_drawing, do: "t"}"}>
+        <div
+          id="hiddengongs-0"
+          phx-hook="Drag"
+          phx-target="#game"
+          class={"hiddengong-tiles dropzone#{if @win_declared_seatno && @win_declared_seatno != @current_user_seatno && @seat.win_expose, do: " exposed-loser-hand"}"}
+        >
+          <%= for tile <- @seat.hiddengongs do %>
+            <.tile id={tile} tile={tile} class="draggable" />
+          <% end %>
+          <div class="dropzone-description">Hidden gong</div>
+        </div>
+
+        <div id="exposed-0" phx-hook="Drag" phx-target="#game" class="exposed-tiles dropzone">
+          <%= for tile <- @seat.exposed do %>
+            <.tile id={tile} tile={tile} class="draggable" />
+          <% end %>
+          <div class="dropzone-description">Exposed tiles</div>
+        </div>
+
+        <div
+          id="wintile-0"
+          phx-hook="Drag"
+          phx-target="#game"
+          class={"wintile-tiles#{if !@win_declared_seatno || @win_declared_seatno == @current_user_seatno, do: " dropzone"}"}
+        >
+          <%= if @seat.wintile do %>
+            <.tile id={@seat.wintile} tile={@seat.wintile} class="cursor-not-allowed" />
+          <% end %>
+          <%= if !@win_declared_seatno || @win_declared_seatno == @current_user_seatno do %>
+            <div class="dropzone-description">Winning tile</div>
+          <% end %>
+        </div>
+
+        <div class="line-break"></div>
+
+        <div
+          id="concealed-0"
+          phx-hook="Drag"
+          phx-target="#game"
+          class={"concealed-tiles dropzone current-user-discarding-#{if @current_user_discarding, do: "t"} enable-pull-from-discards-#{if @available_discard_tile, do: "t"} concealed-loser-hand-#{if @win_declared_seatno && @win_declared_seatno != @current_user_seatno && !@seat.win_expose, do: "t"}"}
+        >
+          <%= for tile <- @seat.concealed do %>
+            <.tile id={tile} tile={tile} class="draggable" />
+          <% end %>
+        </div>
+      </div>
+    </div>
+    """
+  end
 end
